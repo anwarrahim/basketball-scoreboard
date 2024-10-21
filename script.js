0a02d0ab7e45ebfe38605e5dbc739aa8512eab75
Anwar Rahim <anwargithub@gmail.com>
2024-10-22 03:50:02 +0500

    add new project

---
+++ script.js
@@ -0,0 +1,44 @@
+
+
+
+count = 0;
+
+function Home1Increment(){
+
+    count += 1;
+
+    document.getElementById("home-score-num").textContent = count;
+
+}
+
+function Home2Increment(){
+    count += 2;
+
+    document.getElementById("home-score-num").textContent = count;
+}
+
+
+function Home3Increment(){
+    count += 3;
+
+    document.getElementById("home-score-num").textContent = count;
+}
+
+
+//................................Guest........................................
+
+
+function Guest1Increment(){
+    count +=1;
+    document.getElementById("Guest-score-num").textContent = count;
+}
+
+function Guest2Increment(){
+    count +=2;
+    document.getElementById("Guest-score-num").textContent = count;
+}
+
+function Guest3Increment(){
+    count +=3;
+    document.getElementById("Guest-score-num").textContent = count;
+}
