3b5a055533a9c461e004a3ddd7acd2942e28d518
Anwar Rahim <anwargithub@gmail.com>
2024-10-22 03:55:23 +0500

    add new project

--- script.js
+++ script.js
@@ -1,44 +1,44 @@
 
 
 
-count = 0;
+Hcount = 0;
 
 function Home1Increment(){
 
-    count += 1;
+    Hcount += 1;
 
-    document.getElementById("home-score-num").textContent = count;
+    document.getElementById("home-score-num").textContent = Hcount;
 
 }
 
 function Home2Increment(){
-    count += 2;
+    Hcount += 2;
 
-    document.getElementById("home-score-num").textContent = count;
+    document.getElementById("home-score-num").textContent = Hcount;
 }
 
 
 function Home3Increment(){
-    count += 3;
+    Hcount += 3;
 
-    document.getElementById("home-score-num").textContent = count;
+    document.getElementById("home-score-num").textContent = Hcount;
 }
 
 
 //................................Guest........................................
-
+Gcount = 0;
 
 function Guest1Increment(){
-    count +=1;
-    document.getElementById("Guest-score-num").textContent = count;
+    Gcount +=1;
+    document.getElementById("Guest-score-num").textContent = Gcount;
 }
 
 function Guest2Increment(){
-    count +=2;
-    document.getElementById("Guest-score-num").textContent = count;
+    Gcount +=2;
+    document.getElementById("Guest-score-num").textContent = Gcount;
 }
 
 function Guest3Increment(){
-    count +=3;
-    document.getElementById("Guest-score-num").textContent = count;
+    Gcount +=3;
+    document.getElementById("Guest-score-num").textContent = Gcount;
 }
